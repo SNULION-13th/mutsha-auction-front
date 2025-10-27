78583ae80b7a1e4e45a248b8bab7dba0b2a37081
hyuk <paxjc2000@snu.sc.kr>
2025-10-27 13:31:31 +0900

    week3 hw

--- src/Router.tsx
+++ src/Router.tsx
@@ -16,6 +16,9 @@ const PaymentApprovalPage = lazy(
 );
 const PaymentCancelPage = lazy(() => import("@page/Payment/PaymentCancelPage"));
 const PaymentFailPage = lazy(() => import("@page/Payment/PaymentFailPage"));
+const PaymentDetailsPage = lazy(
+  () => import("@/pages/Payment/PaymentDetailsPage"),
+);
 
 const homeRoutes = [
   {
@@ -68,6 +71,10 @@ const paymentRoutes = [
   {
     path: ROUTES.PAYMENT.FAIL,
     element: <PaymentFailPage />,
+  },
+  {
+    path: "/payment/details",
+    element: <PaymentDetailsPage />,
   },
 ];
 
