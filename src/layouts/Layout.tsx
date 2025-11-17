9815c1402693c788e043067969683a471bc47ec4
suhyun <seraphina0911@snu.ac.kr>
2025-11-17 21:03:46 +0900

    sem5 hw

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -5,7 +5,28 @@ import ScrollToTop from "@/components/ScrollToTop";
 import { useEffect } from "react";
 
 export default function Layout() {
-  //TODO: 글로벌 알림 useEffect 구현
+  useEffect(() => {
+    const wsUrl = (import.meta.env.VITE_API_BASE_URL || "").replace(/^http/, "ws");
+    const ws = new WebSocket(`${wsUrl}/ws/notifications/`);
+
+    ws.onopen = () => {
+      console.log("Global notification WebSocket connection established");
+    };
+
+    ws.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      console.log("Received notification:", data);
+      alert(`Notification: ${data.message}`);
+    };
+
+    ws.onclose = (event) => {
+      console.log("Global notification WebSocket connection closed:", event);
+    };
+
+    ws.onerror = (e) => {
+      console.error("WebSocket error:", e);
+    };
+  }, []);
 
   return (
     <div className="w-full bg-bg-default">
