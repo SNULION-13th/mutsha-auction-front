da36b43498c1653b350f3fa4d7e3cd092ddd4b0f
moo <lgmoo2002@gmail.com>
2025-11-20 23:57:33 +0900

    feat: 과제5 clear

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -5,7 +5,40 @@ import ScrollToTop from "@/components/ScrollToTop";
 import { useEffect } from "react";
 
 export default function Layout() {
-  //TODO: 글로벌 알림 useEffect 구현
+  useEffect(() => {
+    const wsUrl = (import.meta.env.VITE_API_BASE_URL || "").replace(
+      /^http/,
+      "ws",
+    );
+    const ws = new WebSocket(`${wsUrl}/ws/notifications/`);
+
+    ws.onopen = () => {
+      console.log("글로벌 알림 WebSocket 연결 성공");
+    };
+
+    ws.onmessage = (event) => {
+      try {
+        const data = JSON.parse(event.data);
+        if (data.message) {
+          alert(data.message); // 팝업으로 띄움
+        }
+      } catch (e) {
+        console.error("알림 메시지 처리 중 오류 발생:", e);
+      }
+    };
+
+    ws.onclose = (event) => {
+      console.log("글로벌 알림 WebSocket 연결 종료:", event);
+    };
+
+    ws.onerror = (e) => {
+      console.error("글로벌 알림 WebSocket 오류:", e);
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, []);
 
   return (
     <div className="w-full bg-bg-default">
