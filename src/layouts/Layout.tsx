334cf5a6031f0ffcd3e8fb34b2caa4e6098b521f
OH <almighty5@snu.ac.kr>
2025-11-28 23:10:57 +0900

    feat: setting for sem6 start

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import ScrollToTop from "@/components/ScrollToTop";
 import { Outlet } from "react-router-dom";
 
 export default function Layout() {
@@ -7,6 +8,7 @@ export default function Layout() {
     <div className="w-full bg-bg-default">
       <Header />
       <main className="min-h-screen pt-22">
+        <ScrollToTop />
         <Outlet />
       </main>
       <Footer />
