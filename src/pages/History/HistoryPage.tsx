d315cb8a5bc89a070d227bb0f36c8cffa863b66a
eileen914 <eileen914@snu.a.c.kr>
2025-11-29 16:55:21 +0900

    sem6 end

--- src/pages/History/HistoryPage.tsx
+++ src/pages/History/HistoryPage.tsx
@@ -1,6 +1,8 @@
 import { getRemainingTime } from "@/utils/datetime";
 import { HistoryCard } from "./components/HistoryCard";
 import { useEffect, useMemo, useRef, useState } from "react";
+import { useTransition } from "@/contexts/TransitionProvider";
+
 import {
   getMyAuctions,
   getMyBids,
@@ -35,6 +37,7 @@ function HistoryPage() {
   const [visibleMine, setVisibleMine] = useState(PAGE_SIZE);
 
   const sentinelRef = useRef<HTMLDivElement | null>(null);
+  const { playTransition } = useTransition();
 
   useEffect(() => {
     async function load() {
@@ -155,6 +158,12 @@ function HistoryPage() {
                         resultText={resultText}
                         win={b.is_winner}
                         buttonText="입찰하기"
+                        onClick={() =>
+                          playTransition({
+                            scenario: scenarioFromBid(b),
+                            to: toRoom,
+                          })
+                        }
                       />
                     );
                   })
--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,4 +1,6 @@
 import { useParams } from "react-router-dom";
+import Lottie from "lottie-react";
+import netflixAnimation from "@/assets/Netflix Logo Swoop.json"; // 여러분의 Lottie 파일을 불러오세요
 
 function NotFoundPage() {
   const param = useParams();
@@ -6,14 +8,15 @@ function NotFoundPage() {
 
   return (
     <div className="w-full h-full py-10 flex flex-col items-center justify-center text-center gap-9">
-      <div className="text-5xl font-bold text-scale-600">404 ERROR</div>
+      {/* <div className="text-5xl font-bold text-scale-600">404 ERROR</div>
       <div className="text-xl text-scale-400">
         앗, 없는 페이지를 찾다니...
         <br />
         {pathName}의 탐험심은 대단하다!
         <br />
         <br />이 곳에서는 뭘 배울 거냐면요...
-      </div>
+      </div> */}
+      <Lottie animationData={netflixAnimation} loop={true} />
     </div>
   );
 }
